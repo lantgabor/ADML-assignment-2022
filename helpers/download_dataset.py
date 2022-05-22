608bd8fe3674898bed2d56ce2da30dc4f19e2ff3
lantgabor <lant.gabor98@gmail.com>
2022-05-22 23:16:52 +0200

    fixes download script

--- helpers/download_dataset.py
+++ helpers/download_dataset.py
@@ -1,21 +1,11 @@
 import os
-import urllib.request
+import requests
 
-urls = [
-    "http://www2.projects.science.uu.nl/shakefive/data/ShakeFive2.background.tar.bz2",
-    "http://www2.projects.science.uu.nl/shakefive/data/ShakeFive2.metadata.tar.bz2",
-    "http://www2.projects.science.uu.nl/shakefive/data/ShakeFive2.videos.tar",
-    "http://www2.projects.science.uu.nl/shakefive/data/ShakeFive2.code.tar.gz",
-]
+for url in open('./helpers/urls.txt'):
+    
+    name = os.path.basename(url)
+    filename = os.path.join('./data', name)
 
-# For every line in the file
-for url in urls:
-    # Split on the rightmost / and take everything on the right side of tha
-    name = url.rsplit("/", 1)[-1]
-
-    # Combine the name and the downloads directory to get the local filename
-    filename = os.path.join(url, name)
-
-    # Download the file if it does not exist
     if not os.path.isfile(filename):
-        urllib.request.urlretrieve(url, filename)
+        r = requests.get(url)
+        open(filename, 'wb').write(r.content)
