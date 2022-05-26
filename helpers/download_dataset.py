d7d5e701beef98e87b183a6207a66e0c9cf52a9a
lantgabor <lant.gabor98@gmail.com>
2022-05-26 09:52:56 +0200

    moves data to seperate folder

--- helpers/download_dataset.py
+++ helpers/download_dataset.py
@@ -1,11 +1,15 @@
 import os
 import requests
 
-for url in open('./helpers/urls.txt'):
-    
-    name = os.path.basename(url)
-    filename = os.path.join('./data', name)
+def download_data(urls, out_dir):
+    for url in open(urls):
+        name = os.path.basename(url)
+        filename = os.path.join(out_dir, name)
 
-    if not os.path.isfile(filename):
-        r = requests.get(url)
-        open(filename, 'wb').write(r.content)
+        if not os.path.isfile(filename):
+            r = requests.get(url)
+            open(filename, 'wb').write(r.content)
+
+
+download_data('./helpers/shakefive2.txt', './data/shakefive2')
+download_data('./helpers/tv_human.txt', './data/tv_human')
