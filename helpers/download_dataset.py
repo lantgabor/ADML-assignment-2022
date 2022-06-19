e1c71a55724f92c9a32fcdde9563b5428a3f05d5
lantgabor <lant.gabor98@gmail.com>
2022-06-19 21:27:20 +0200

    adds extract fn

--- helpers/download_dataset.py
+++ helpers/download_dataset.py
@@ -1,15 +1,45 @@
 import os
-import requests
+import wget
+import tarfile
 
-def download_data(urls, out_dir):
+
+def extract_data(path, filename):
+    if filename.endswith(".bz2\n"):
+        print("Extracting file: ", filename, "bz2")
+        with tarfile.open(filename, "r:bz2") as tar:
+            tar.extractall(path=os.path.join(path, "out"))
+            tar.close()
+    elif filename.endswith(".gz\n"):
+        print("Extracting file: ", filename, "gz")
+        with tarfile.open(filename, "r:gz") as tar:
+            tar.extractall(path=os.path.join(path, "out"))
+            tar.close()
+    elif filename.endswith(".tar\n"):
+        print("Extracting file: ", filename, "tar")
+        with tarfile.open(filename, "r:") as tar:
+            tar.extractall(path=os.path.join(path, "out"))
+            tar.close()
+    else:
+        print(filename)
+
+
+def download_data(urls, path):
+
+    # create data directory
+    final_directory = os.path.join(os.getcwd(), path)
+    os.makedirs(final_directory, exist_ok=True)
+
+    # download datasets
     for url in open(urls):
         name = os.path.basename(url)
-        filename = os.path.join(out_dir, name)
+        filename = os.path.join(path, name)
 
         if not os.path.isfile(filename):
-            r = requests.get(url)
-            open(filename, 'wb').write(r.content)
+            # print("Downloading: {}".format(filename))
+            wget.download(url, out=filename)
+
+            extract_data(path, filename)
 
 
-download_data('./helpers/shakefive2.txt', './data/shakefive2')
-download_data('./helpers/tv_human.txt', './data/tv_human')
+download_data("./helpers/shakefive2.txt", "./data/shakefive2")
+download_data("./helpers/tv_human.txt", "./data/tv_human")
