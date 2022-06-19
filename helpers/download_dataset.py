be402f38c03d940e33a051d83d757710fc5c2394
lantgabor <lant.gabor98@gmail.com>
2022-06-19 22:47:00 +0200

    adds progress bar

--- helpers/download_dataset.py
+++ helpers/download_dataset.py
@@ -1,23 +1,29 @@
 import os
+from numpy import iterable
 import wget
 import tarfile
+from tqdm import tqdm
 
 
 def extract_data(path, filename):
-    if filename.endswith(".bz2\n"):
-        print("Extracting file: ", filename, "bz2")
+
+    if filename.endswith(".bz2"):
+        # print("Extracting file: ", filename, "bz2")
         with tarfile.open(filename, "r:bz2") as tar:
-            tar.extractall(path=os.path.join(path, "out"))
+            for member in tqdm(iterable=tar.getmembers(), total=len(tar.getmembers())):
+                tar.extract(member=member, path=os.path.join(path, "out"))
             tar.close()
-    elif filename.endswith(".gz\n"):
-        print("Extracting file: ", filename, "gz")
+    elif filename.endswith(".gz"):
+        # print("Extracting file: ", filename, "gz")
         with tarfile.open(filename, "r:gz") as tar:
-            tar.extractall(path=os.path.join(path, "out"))
+            for member in tqdm(iterable=tar.getmembers(), total=len(tar.getmembers())):
+                tar.extract(member=member, path=os.path.join(path, "out"))
             tar.close()
-    elif filename.endswith(".tar\n"):
-        print("Extracting file: ", filename, "tar")
+    elif filename.endswith(".tar"):
+        # print("Extracting file: ", filename, "tar")
         with tarfile.open(filename, "r:") as tar:
-            tar.extractall(path=os.path.join(path, "out"))
+            for member in tqdm(iterable=tar.getmembers(), total=len(tar.getmembers())):
+                tar.extract(member=member, path=os.path.join(path, "out"))
             tar.close()
     else:
         print(filename)
@@ -31,14 +37,15 @@ def download_data(urls, path):
 
     # download datasets
     for url in open(urls):
+        url = url.strip()
         name = os.path.basename(url)
         filename = os.path.join(path, name)
 
         if not os.path.isfile(filename):
-            # print("Downloading: {}".format(filename))
+            print("Downloading: {}\n".format(filename))
             wget.download(url, out=filename)
 
-            extract_data(path, filename)
+        extract_data(path, filename)
 
 
 download_data("./helpers/shakefive2.txt", "./data/shakefive2")
